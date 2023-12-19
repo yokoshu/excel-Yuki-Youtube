ffb80c8faac521d1d2aa13c3b38b0de3cb40f997
yokoshu <130563994+yokoshu@users.noreply.github.com>
2023-12-19 21:35:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -184,10 +184,10 @@ template = Jinja2Templates(directory='templates').TemplateResponse
 @app.get("/", response_class=HTMLResponse)
 def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
     if check_cokie(yuki):
-        response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
+        response.set_cookie("20221202","True",max_age=60 * 60 * 24 * 7)
         return template("home.html",{"request": request})
     print(check_cokie(yuki))
-    return redirect("/word")
+    return redirect("/excel")
 
 @app.get('/watch', response_class=HTMLResponse)
 def video(v:str,response: Response,request: Request,yuki: Union[str] = Cookie(None),proxy: Union[str] = Cookie(None)):
